ddaf714588b90d3a69a0988a37d26450cdca8ffc
ambar <ambar.lee@gmail.com>
2018-03-09 13:13:50 +0800

    allow mina.config.js

--- packages/mina-scripts/scripts/utils/resolveConfig.js
+++ packages/mina-scripts/scripts/utils/resolveConfig.js
@@ -1,6 +1,34 @@
-const resolveConfig = () => {
-  const baseConfig = require('../../config/webpack.config.base')
-  return {webpack: baseConfig}
+const fs = require('fs')
+const path = require('path')
+const {mergeWith, partialRight} = require('lodash')
+
+const deepMerge = partialRight(mergeWith, (objValue, srcValue) => {
+  if (Array.isArray(objValue)) {
+    return objValue.concat(srcValue)
+  }
+})
+
+const loadUserConfig = () => {
+  const configFile = path.resolve(process.cwd(), 'mina.config.js')
+  if (fs.existsSync(configFile)) {
+    return require(configFile)
+  }
+
+  return null
+}
+
+const resolveConfig = async () => {
+  let baseConfig = {
+    webpack: require('../../config/webpack.config.base'),
+  }
+
+  const userConfig = loadUserConfig() || {}
+
+  if (typeof userConfig.overrides === 'function') {
+    baseConfig = userConfig.overrides(baseConfig)
+  }
+
+  return deepMerge(baseConfig, userConfig)
 }
 
 module.exports = resolveConfig
